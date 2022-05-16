a9325cb1ce3676078d642407122ed4ecaba62a97
Nelson Chaves <nelsonchavespro@gmail.com>
2022-05-16 11:52:50 -0700

    first commit

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe 'Card' do
 
 end
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M master
+git remote add origin git@github.com:nelsonchaves/rspec-course.git
+git push -u origin master
